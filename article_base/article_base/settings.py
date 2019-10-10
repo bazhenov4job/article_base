3e7d9b06d335e8f9134c7ce72b3cde3a4f14a9c8
bazhenov4job <bazhenov4job@gmail.com>
2019-10-10 17:45:14 +0300

    Changed loation of mainapp

--- article_base/article_base/settings.py
+++ article_base/article_base/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainapp.apps.MainappConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'article_base.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.path.join(BASE_DIR, 'db.mysql'),
+        'USER': 'bazhenov',
+        'PASSWORD': '0D7762fe4c0!',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
