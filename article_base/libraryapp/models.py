04add8b81ad0bcff063eff3862d28b9e9430d2ef
bazhenov4job <bazhenov4job@gmail.com>
2019-10-22 14:33:58 +0300

    Added models in library for Themes and Sources

--- article_base/libraryapp/models.py
+++ article_base/libraryapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from libraryapp.views import load_themes
 
 # Create your models here.
+
+
+class Themes(models.Model):
+    themes = load_themes()
+    for theme in themes:
+        locals()[theme] = models.BooleanField(verbose_name=themes[theme], default=False)
+
+
+class Sources(models.Model):
+    source = models.CharField(verbose_name="Источники", max_length=64, unique=True)
--- article_base/libraryapp/views.py
+++ article_base/libraryapp/views.py
@@ -6,11 +6,15 @@ with open('static/json/links.json', 'r', encoding="UTF-8") as read_file:
     links = json.load(read_file)['links']
 
 
-def library(request):
-    title = 'Библиотека'
+def load_themes():
     with open('static/json/themes.json', 'r', encoding='cp1251') as themes_json:
         themes = json.load(themes_json)['themes']
+        return themes
 
+
+def library(request):
+    title = 'Библиотека'
+    themes = load_themes()
     sources = ['iepc',
                'phys of plasmas',
                'prop and power',
