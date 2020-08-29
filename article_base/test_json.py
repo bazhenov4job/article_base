4dd3c3a4b377d8c689433656573aa2290508421c
bazhenov4job <46837972+bazhenov4job@users.noreply.github.com>
2020-08-29 18:14:23 +0300

    deleted unsuporte on server console output functions

--- article_base/test_json.py
+++ article_base/test_json.py
@@ -4,7 +4,6 @@ import json
 with open('themes.txt', 'r', encoding='UTF-8') as themes_file:
     themes = {"themes": themes_file.read().split('\n')}
 
-print(themes)
 
 with open('static/json/themes.json', 'w', encoding='cp1251') as jthemes:
     json.dump(themes, jthemes, ensure_ascii=False, indent=4)
